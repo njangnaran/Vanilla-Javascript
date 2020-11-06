fe642e8569df0162c6b81566388aea5f9dece7a4
njangnaran <njangnaran@naver.com>
2020-11-06 17:20:16 +0900

    직관적으로 바꿈

--- Modal.js
+++ Modal.js
@@ -1,13 +1,11 @@
 const openButton = document.getElementById("open");
 const modal = document.querySelector(".modal");
-const overlay = modal.querySelector(".modal__overlay");
-const closeBtn = modal.querySelector("button");
+const closeBtn = modal.querySelector("#exitbtn");
 const openModal = () => {
   modal.classList.remove("hidden");
 };
 const closeModal = () => {
   modal.classList.add("hidden");
 };
-overlay.addEventListener("click", closeModal);
 closeBtn.addEventListener("click", closeModal);
 openButton.addEventListener("click", openModal);
